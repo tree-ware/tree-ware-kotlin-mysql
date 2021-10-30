87d6f3097ab6e84fbe03acf79ea6958e114ece75
Deepak Nulu <deepak@celona.io>
2021-10-30 15:32:06 -0700

    Generate database & table creation commands
    Resolves #7
    Resolves #8

--- src/main/kotlin/org/treeWare/mySql/aux/MySqlMetaModelMap.kt
+++ src/main/kotlin/org/treeWare/mySql/aux/MySqlMetaModelMap.kt
@@ -1,5 +1,6 @@
 package org.treeWare.mySql.aux
 
 class MySqlMetaModelMap {
+    var tablePrefix: String? = null
     var tableName: String? = null
 }
--- src/main/kotlin/org/treeWare/mySql/aux/MySqlMetaModelMapEncoder.kt
+++ src/main/kotlin/org/treeWare/mySql/aux/MySqlMetaModelMapEncoder.kt
@@ -9,6 +9,9 @@ class MySqlMetaModelMapEncoder : AuxEncoder {
             val auxFieldName = wireFormatEncoder.getAuxFieldName(fieldName, auxName)
             wireFormatEncoder.encodeObjectStart(auxFieldName)
             val mySqlAux = aux as MySqlMetaModelMap
+            mySqlAux.tablePrefix?.also {
+                wireFormatEncoder.encodeStringField(MY_SQL_META_MODEL_MAP_CODEC_TABLE_PREFIX, it)
+            }
             mySqlAux.tableName?.also { wireFormatEncoder.encodeStringField(MY_SQL_META_MODEL_MAP_CODEC_TABLE_NAME, it) }
             wireFormatEncoder.encodeObjectEnd()
         }
