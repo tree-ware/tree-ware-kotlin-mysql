092fa1e9858b6d8cad84453edea5dbf001484210
Deepak Nulu <deepak@celona.io>
2021-11-28 23:07:58 -0800

    Use newMetaModel() from core main

--- src/test/kotlin/org/treeWare/metaModel/MySqlAddressBookMetaModel.kt
+++ src/test/kotlin/org/treeWare/metaModel/MySqlAddressBookMetaModel.kt
@@ -1,10 +1,8 @@
 package org.treeWare.metaModel
 
-import org.apache.logging.log4j.LogManager
 import org.treeWare.model.core.Cipher
 import org.treeWare.model.core.Hasher
-import org.treeWare.model.core.MutableMainModel
-import org.treeWare.model.decoder.stateMachine.MultiAuxDecodingStateMachineFactory
+import org.treeWare.model.core.MainModel
 import org.treeWare.mySql.aux.MySqlMetaModelAuxPlugin
 
 val MY_SQL_ADDRESS_BOOK_META_MODEL_FILES = listOf(
@@ -13,19 +11,10 @@ val MY_SQL_ADDRESS_BOOK_META_MODEL_FILES = listOf(
     "metaModel/my_sql_address_book_city.json",
 )
 
-fun newMySqlAddressBookMetaModel(environment: String, hasher: Hasher?, cipher: Cipher?): MutableMainModel {
-    val mySqlMetaModelAuxPlugin = MySqlMetaModelAuxPlugin(environment)
-    val metaModel = newMetaModelFromFiles(
-        MY_SQL_ADDRESS_BOOK_META_MODEL_FILES,
-        hasher,
-        cipher,
-        MultiAuxDecodingStateMachineFactory(mySqlMetaModelAuxPlugin.auxName to mySqlMetaModelAuxPlugin.auxDecodingStateMachineFactory)
-    )
-    val errors = mySqlMetaModelAuxPlugin.validate(metaModel)
-    if (errors.isNotEmpty()) {
-        val logger = LogManager.getLogger()
-        errors.forEach { logger.error(it) }
-        throw IllegalStateException("Address-book meta-model MySQL map is not valid")
-    }
-    return metaModel
-}
+fun newMySqlAddressBookMetaModel(environment: String, hasher: Hasher?, cipher: Cipher?): MainModel = newMetaModel(
+    MY_SQL_ADDRESS_BOOK_META_MODEL_FILES,
+    false,
+    hasher,
+    cipher,
+    listOf(MySqlMetaModelAuxPlugin(environment))
+)
