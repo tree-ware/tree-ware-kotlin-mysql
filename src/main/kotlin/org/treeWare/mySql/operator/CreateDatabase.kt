24722a0186151f39ce036dfdf34192f03e127e16
Deepak Nulu <deepak@celona.io>
2021-12-07 18:17:09 -0800

    Add an option to log the MySQL create commands

--- src/main/kotlin/org/treeWare/mySql/operator/CreateDatabase.kt
+++ src/main/kotlin/org/treeWare/mySql/operator/CreateDatabase.kt
@@ -1,11 +1,14 @@
 package org.treeWare.mySql.operator
 
+import org.apache.logging.log4j.LogManager
 import org.treeWare.model.core.MainModel
 import java.sql.Connection
 
-fun createDatabase(mainMeta: MainModel, connection: Connection) {
+fun createDatabase(mainMeta: MainModel, connection: Connection, logCommands: Boolean = false) {
+    val logger = if (logCommands) LogManager.getLogger() else null
     val commands = generateCreateCommands(mainMeta)
     commands.forEach { command ->
+        logger?.info(command)
         val statement = connection.createStatement()
         statement.executeUpdate(command)
         statement.close()
