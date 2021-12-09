4204bce6ff3f8c1197be2b249a52a656be3bad5e
Deepak Nulu <deepak@celona.io>
2021-12-08 20:34:32 -0800

    Include keys in 'ON DUPLICATE KEY UPDATE' clause
    Resolves #22

--- src/main/kotlin/org/treeWare/mySql/operator/Set.kt
+++ src/main/kotlin/org/treeWare/mySql/operator/Set.kt
@@ -1,14 +1,24 @@
 package org.treeWare.mySql.operator
 
+import org.apache.logging.log4j.LogManager
 import org.treeWare.model.core.MainModel
 import java.sql.Connection
 
+private val logger = LogManager.getLogger()
+
 // TODO(deepak-nulu): return a model with error aux
-fun set(mainModel: MainModel, connection: Connection) {
+fun set(mainModel: MainModel, connection: Connection, logCommands: Boolean = false) {
     val commands = generateSetCommands(mainModel)
     commands.forEach { command ->
+        if (logCommands) logger.info(command)
         val statement = connection.createStatement()
-        statement.executeUpdate(command)
-        statement.close()
+        try {
+            statement.executeUpdate(command)
+        } catch (e: Exception) {
+            logger.error("Exception for SQL command: $command")
+            throw e
+        } finally {
+            statement.close()
+        }
     }
 }
