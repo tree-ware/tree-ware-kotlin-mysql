ea2ce759ccd6462a5ff776a7fb11708852fa5d50
Deepak Nulu <deepak@celona.io>
2022-05-09 17:14:14 -0700

    Use one more entity & association in set-tests
    This does not help set-tests, but it will help get-tests. It will help
    test a get-request with multiple specific entities and a wildcard
    entity (at least 3 entities are required in the database for this).

--- src/test/resources/operator/my_sql_address_book_1_set_delete_bottoms_up_commands.sql
+++ src/test/resources/operator/my_sql_address_book_1_set_delete_bottoms_up_commands.sql
@@ -4,12 +4,18 @@ UPDATE test$address_book.main$address_book_person
 UPDATE test$address_book.main$address_book_relation
   SET person$id = NULL
   WHERE id = UUID_TO_BIN('05ade278-4b44-43da-a0cc-14463854e397') AND entity_path$ = '/address_book/person[cc477201-48ec-4367-83a4-7fdbd92f8a6f]/relation[05ade278-4b44-43da-a0cc-14463854e397]';
+UPDATE test$address_book.main$address_book_relation
+  SET person$id = NULL
+  WHERE id = UUID_TO_BIN('3c71ede8-8ded-4038-b6e9-dcc4a0f3a8ce') AND entity_path$ = '/address_book/person[cc477201-48ec-4367-83a4-7fdbd92f8a6f]/relation[3c71ede8-8ded-4038-b6e9-dcc4a0f3a8ce]';
 UPDATE test$address_book.main$address_book_person
   SET self$id = NULL
   WHERE id = UUID_TO_BIN('a8aacf55-7810-4b43-afe5-4344f25435fd') AND entity_path$ = '/address_book/person[a8aacf55-7810-4b43-afe5-4344f25435fd]';
 UPDATE test$address_book.main$address_book_relation
   SET person$id = NULL
   WHERE id = UUID_TO_BIN('16634916-8f83-4376-ad42-37038e108a0b') AND entity_path$ = '/address_book/person[a8aacf55-7810-4b43-afe5-4344f25435fd]/relation[16634916-8f83-4376-ad42-37038e108a0b]';
+UPDATE test$address_book.main$address_book_person
+  SET self$id = NULL
+  WHERE id = UUID_TO_BIN('ec983c56-320f-4d66-9dde-f180e8ac3807') AND entity_path$ = '/address_book/person[ec983c56-320f-4d66-9dde-f180e8ac3807]';
 UPDATE test$address_book.main$address_book_person
   SET self$id = NULL
   WHERE id = UUID_TO_BIN('546a4982-b39a-4d01-aeb3-22d60c6963c0') AND entity_path$ = '/address_book/groups[ca0a22e8-c300-4347-91b0-167a5f6f4f9a]/sub_groups[fe2aa774-e1fe-4680-a439-8bd1d0eb4abc]/persons[546a4982-b39a-4d01-aeb3-22d60c6963c0]';
@@ -46,6 +52,8 @@ DELETE FROM test$address_book.main$person_group
   WHERE id = UUID_TO_BIN('fe2aa774-e1fe-4680-a439-8bd1d0eb4abc') AND entity_path$ = '/address_book/groups[ca0a22e8-c300-4347-91b0-167a5f6f4f9a]/sub_groups[fe2aa774-e1fe-4680-a439-8bd1d0eb4abc]';
 DELETE FROM test$address_book.main$person_group
   WHERE id = UUID_TO_BIN('ca0a22e8-c300-4347-91b0-167a5f6f4f9a') AND entity_path$ = '/address_book/groups[ca0a22e8-c300-4347-91b0-167a5f6f4f9a]';
+DELETE FROM test$address_book.main$address_book_person
+  WHERE id = UUID_TO_BIN('ec983c56-320f-4d66-9dde-f180e8ac3807') AND entity_path$ = '/address_book/person[ec983c56-320f-4d66-9dde-f180e8ac3807]';
 DELETE FROM test$address_book.crypto$secret
   WHERE entity_path$ = '/address_book/person[a8aacf55-7810-4b43-afe5-4344f25435fd]/secret';
 DELETE FROM test$address_book.crypto$password
@@ -58,6 +66,8 @@ DELETE FROM test$address_book.crypto$secret
   WHERE entity_path$ = '/address_book/person[cc477201-48ec-4367-83a4-7fdbd92f8a6f]/secret';
 DELETE FROM test$address_book.crypto$password
   WHERE entity_path$ = '/address_book/person[cc477201-48ec-4367-83a4-7fdbd92f8a6f]/password';
+DELETE FROM test$address_book.main$address_book_relation
+  WHERE id = UUID_TO_BIN('3c71ede8-8ded-4038-b6e9-dcc4a0f3a8ce') AND entity_path$ = '/address_book/person[cc477201-48ec-4367-83a4-7fdbd92f8a6f]/relation[3c71ede8-8ded-4038-b6e9-dcc4a0f3a8ce]';
 DELETE FROM test$address_book.main$address_book_relation
   WHERE id = UUID_TO_BIN('05ade278-4b44-43da-a0cc-14463854e397') AND entity_path$ = '/address_book/person[cc477201-48ec-4367-83a4-7fdbd92f8a6f]/relation[05ade278-4b44-43da-a0cc-14463854e397]';
 DELETE FROM test$address_book.main$address_book_person
