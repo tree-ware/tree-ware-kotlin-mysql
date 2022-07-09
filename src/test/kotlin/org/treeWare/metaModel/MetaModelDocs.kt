49b93caa531103cc6f8781d8820aed8084c24589
RogerB-ce <roger.brockett@celona.io>
2022-07-08 17:05:33 -0700

    Replace repetitive dot encoding code with function

--- src/test/kotlin/org/treeWare/metaModel/MetaModelDocs.kt
+++ src/test/kotlin/org/treeWare/metaModel/MetaModelDocs.kt
@@ -1,7 +1,6 @@
 package org.treeWare.metaModel
 
 import org.treeWare.metaModel.encoder.encodeDot
-import java.io.File
 import kotlin.test.Test
 
 // TODO(deepak-nulu): make doc generation a gradle task
@@ -9,12 +8,6 @@ import kotlin.test.Test
 class MetaModelDocs {
     @Test
     fun `Generate MySQL address-book meta-model docs`() {
-        val mainMetaName = getMainMetaName(mySqlAddressBookMetaModel)
-        val fileName = "${mainMetaName}_meta_model"
-        val fileWriter = File("${fileName}.dot").bufferedWriter()
-        encodeDot(mySqlAddressBookMetaModel, fileWriter)
-        fileWriter.flush()
-
-        Runtime.getRuntime().exec("dot -Tpng ${fileName}.dot -o ${fileName}.png").waitFor()
+        encodeDot(mySqlAddressBookMetaModel)
     }
 }
