fa4a2665433d25d944abb28e31e76926406d8255
Deepak Nulu <4168103+deepak-nulu@users.noreply.github.com>
2024-10-12 23:11:10 -0700

    Drop main-models & list-models 4/N
    These have been dropped in core, so they need
    to be dropped everywhere.

--- src/main/kotlin/org/treeWare/mySql/ddl/traversal/Leader1DdlAdapter.kt
+++ src/main/kotlin/org/treeWare/mySql/ddl/traversal/Leader1DdlAdapter.kt
@@ -2,6 +2,7 @@ package org.treeWare.mySql.ddl.traversal
 
 import org.treeWare.model.core.*
 import org.treeWare.model.traversal.IllegalStateLeader1ModelVisitor
+import org.treeWare.sql.ddl.*
 
 class Leader1DdlAdapter<Return>(
     private val adaptee: Leader1DdlVisitor<Return>,
@@ -9,27 +10,27 @@ class Leader1DdlAdapter<Return>(
 ) : IllegalStateLeader1ModelVisitor<Return>() {
     override fun visitEntity(leaderEntity1: EntityModel): Return =
         when (val entityMetaName = leaderEntity1.getMetaResolved()?.fullName) {
-            "/org.tree_ware.sql.ddl/ddl" -> defaultVisitReturn
-            "/org.tree_ware.sql.ddl/database" -> adaptee.visitDatabase(leaderEntity1)
-            "/org.tree_ware.sql.ddl/table" -> adaptee.visitTable(leaderEntity1)
-            "/org.tree_ware.sql.ddl/column" -> adaptee.visitColumn(leaderEntity1)
-            "/org.tree_ware.sql.ddl/index" -> adaptee.visitIndex(leaderEntity1)
-            "/org.tree_ware.sql.ddl/index_column" -> adaptee.visitIndexColumn(leaderEntity1)
-            "/org.tree_ware.sql.ddl/foreign_key" -> adaptee.visitForeignKey(leaderEntity1)
-            "/org.tree_ware.sql.ddl/key_mapping" -> adaptee.visitKeyMapping(leaderEntity1)
+            "/org.tree_ware.sql.ddl/ddl_root" -> defaultVisitReturn
+            "/org.tree_ware.sql.ddl/database" -> adaptee.visitDatabase(leaderEntity1 as Database)
+            "/org.tree_ware.sql.ddl/table" -> adaptee.visitTable(leaderEntity1 as Table)
+            "/org.tree_ware.sql.ddl/column" -> adaptee.visitColumn(leaderEntity1 as Column)
+            "/org.tree_ware.sql.ddl/index" -> adaptee.visitIndex(leaderEntity1 as Index)
+            "/org.tree_ware.sql.ddl/index_column" -> adaptee.visitIndexColumn(leaderEntity1 as IndexColumn)
+            "/org.tree_ware.sql.ddl/foreign_key" -> adaptee.visitForeignKey(leaderEntity1 as ForeignKey)
+            "/org.tree_ware.sql.ddl/key_mapping" -> adaptee.visitKeyMapping(leaderEntity1 as KeyMapping)
             else -> throw IllegalStateException("Illegal entityMetaName: $entityMetaName")
         }
 
     override fun leaveEntity(leaderEntity1: EntityModel) =
         when (val entityMetaName = leaderEntity1.getMetaResolved()?.fullName) {
-            "/org.tree_ware.sql.ddl/ddl" -> Unit
-            "/org.tree_ware.sql.ddl/database" -> adaptee.leaveDatabase(leaderEntity1)
-            "/org.tree_ware.sql.ddl/table" -> adaptee.leaveTable(leaderEntity1)
-            "/org.tree_ware.sql.ddl/column" -> adaptee.leaveColumn(leaderEntity1)
-            "/org.tree_ware.sql.ddl/index" -> adaptee.leaveIndex(leaderEntity1)
-            "/org.tree_ware.sql.ddl/index_column" -> adaptee.leaveIndexColumn(leaderEntity1)
-            "/org.tree_ware.sql.ddl/foreign_key" -> adaptee.leaveForeignKey(leaderEntity1)
-            "/org.tree_ware.sql.ddl/key_mapping" -> adaptee.leaveKeyMapping(leaderEntity1)
+            "/org.tree_ware.sql.ddl/ddl_root" -> Unit
+            "/org.tree_ware.sql.ddl/database" -> adaptee.leaveDatabase(leaderEntity1 as Database)
+            "/org.tree_ware.sql.ddl/table" -> adaptee.leaveTable(leaderEntity1 as Table)
+            "/org.tree_ware.sql.ddl/column" -> adaptee.leaveColumn(leaderEntity1 as Column)
+            "/org.tree_ware.sql.ddl/index" -> adaptee.leaveIndex(leaderEntity1 as Index)
+            "/org.tree_ware.sql.ddl/index_column" -> adaptee.leaveIndexColumn(leaderEntity1 as IndexColumn)
+            "/org.tree_ware.sql.ddl/foreign_key" -> adaptee.leaveForeignKey(leaderEntity1 as ForeignKey)
+            "/org.tree_ware.sql.ddl/key_mapping" -> adaptee.leaveKeyMapping(leaderEntity1 as KeyMapping)
             else -> throw IllegalStateException("Illegal entityMetaName: $entityMetaName")
         }
 
--- src/main/kotlin/org/treeWare/mySql/ddl/traversal/Leader1DdlVisitor.kt
+++ src/main/kotlin/org/treeWare/mySql/ddl/traversal/Leader1DdlVisitor.kt
@@ -1,26 +1,26 @@
 package org.treeWare.mySql.ddl.traversal
 
-import org.treeWare.model.core.EntityModel
+import org.treeWare.sql.ddl.*
 
 interface Leader1DdlVisitor<Return> {
-    fun visitDatabase(leaderDatabase1: EntityModel): Return
-    fun leaveDatabase(leaderDatabase1: EntityModel)
+    fun visitDatabase(leaderDatabase1: Database): Return
+    fun leaveDatabase(leaderDatabase1: Database)
 
-    fun visitTable(leaderTable1: EntityModel): Return
-    fun leaveTable(leaderTable1: EntityModel)
+    fun visitTable(leaderTable1: Table): Return
+    fun leaveTable(leaderTable1: Table)
 
-    fun visitColumn(leaderColumn1: EntityModel): Return
-    fun leaveColumn(leaderColumn1: EntityModel)
+    fun visitColumn(leaderColumn1: Column): Return
+    fun leaveColumn(leaderColumn1: Column)
 
-    fun visitIndex(leaderIndex1: EntityModel): Return
-    fun leaveIndex(leaderIndex1: EntityModel)
+    fun visitIndex(leaderIndex1: Index): Return
+    fun leaveIndex(leaderIndex1: Index)
 
-    fun visitIndexColumn(leaderIndexColumn1: EntityModel): Return
-    fun leaveIndexColumn(leaderIndexColumn1: EntityModel)
+    fun visitIndexColumn(leaderIndexColumn1: IndexColumn): Return
+    fun leaveIndexColumn(leaderIndexColumn1: IndexColumn)
 
-    fun visitForeignKey(leaderForeignKey1: EntityModel): Return
-    fun leaveForeignKey(leaderForeignKey1: EntityModel)
+    fun visitForeignKey(leaderForeignKey1: ForeignKey): Return
+    fun leaveForeignKey(leaderForeignKey1: ForeignKey)
 
-    fun visitKeyMapping(leaderKeyMapping1: EntityModel): Return
-    fun leaveKeyMapping(leaderKeyMapping1: EntityModel)
+    fun visitKeyMapping(leaderKeyMapping1: KeyMapping): Return
+    fun leaveKeyMapping(leaderKeyMapping1: KeyMapping)
 }
